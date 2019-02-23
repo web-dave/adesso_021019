baaf236b31d39621ea7ffe2ff720398616bd3002
Dave <dave@webdave.de>
2019-02-23 11:26:34 +0100

    spy

--- src/app/book/book-list/book-list.component.spec.ts
+++ src/app/book/book-list/book-list.component.spec.ts
@@ -1,29 +1,40 @@
 import { RouterTestingModule } from '@angular/router/testing';
-import { BookStaticAsyncDataService } from './../shared/book-static-async-data.service';
+import {
+  BookStaticAsyncDataService as stubsrc,
+  BookStaticAsyncDataService
+} from './../shared/book-static-async-data.service';
 import { BookDataService } from '../shared/book-data.service';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { BookListComponent } from './book-list.component';
 import { DebugElement } from '@angular/core';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { Observable } from 'rxjs';
 
-fdescribe('BookListComponent', () => {
+describe('BookListComponent', () => {
   let component: BookListComponent;
   let fixture: ComponentFixture<BookListComponent>;
   let compiled;
   let service: BookStaticAsyncDataService;
+  let mySpy;
 
+  // providers: [
+  //   {
+  //     provide: BookDataService,
+  //     useClass: BookStaticAsyncDataService
+  //   }
+  // ]
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [BookListComponent],
-      imports: [RouterTestingModule.withRoutes([])],
-      providers: [
-        {
-          provide: BookDataService,
-          useClass: BookStaticAsyncDataService
-        }
-      ]
+      imports: [RouterTestingModule.withRoutes([]), HttpClientTestingModule],
+      providers: [BookDataService]
     }).compileComponents();
     service = TestBed.get(BookDataService);
+    const srv = new BookStaticAsyncDataService();
+    mySpy = spyOn(service, 'getBooks').and.returnValue(
+      Observable.of(srv.staticBookData)
+    );
   });
 
   beforeEach(() => {
@@ -38,8 +49,9 @@ fdescribe('BookListComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should recieve 3 Books', () => {
+  fit('should recieve 3 Books', () => {
     expect(compiled.querySelectorAll('.book-row').length).toBe(3);
+    expect(service.getBooks).toHaveBeenCalled();
   });
 
   it('should display the title of each book', () => {
